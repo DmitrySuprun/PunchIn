58c9584061b1d6c949305cd75369d43b982701ca
Dmitry <suprun1212@gmail.com>
2023-03-14 13:36:34 +0300

    Update config from Chris video

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,9 +7,10 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
-        postgresConfig.tlsConfiguration = .makeClientConfiguration()
-        postgresConfig.tlsConfiguration?.certificateVerification = .none
+    if let urlString = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: urlString) {
+        var tlsConfig = TLSConfiguration.makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        postgresConfig.tlsConfiguration = tlsConfig
         app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
     } else {
         app.databases.use(.postgres(
